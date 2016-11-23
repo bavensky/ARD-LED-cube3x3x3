537a80a6a946270b9d5aaf430447265922805458
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-11-23 15:23:11 +0700

    no message

---
+++ Cube3x3x3/Cube3x3x3.ino
@@ -0,0 +1,25 @@
+#define led1 2
+#define led1 3
+#define led1 4
+#define led1 5
+#define led1 6
+#define led1 7
+#define led1 8
+#define led1 9
+#define led1 10
+#define floor1 11
+#define floor2 12
+#define floor3 13
+
+
+void setup() {
+  for(int i=2; i<=13; i++)   {
+    pinMode(i, OUTPUT);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
