6538bce32cef3e1051c88618c115dbdf56ad2cd2
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2016-11-23 19:57:44 +0700

    no message

--- Cube3x3x3/Cube3x3x3.ino
+++ Cube3x3x3/Cube3x3x3.ino
@@ -1,25 +1,38 @@
 #define led1 2
-#define led1 3
-#define led1 4
-#define led1 5
-#define led1 6
-#define led1 7
-#define led1 8
-#define led1 9
-#define led1 10
+#define led2 3
+#define led3 4
+#define led4 5
+#define led5 6
+#define led6 7
+#define led7 8
+#define led8 9
+#define led9 10
 #define floor1 11
 #define floor2 12
 #define floor3 13
 
 
 void setup() {
-  for(int i=2; i<=13; i++)   {
+  for (int i = 2; i <= 13; i++)   {
     pinMode(i, OUTPUT);
   }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(floor1, LOW);
+  digitalWrite(floor2, LOW);
+  digitalWrite(floor3, LOW);
 
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  digitalWrite(led7, HIGH);
+  digitalWrite(led8, HIGH);
+  digitalWrite(led9, HIGH);
+
+  delay(100);
 }
